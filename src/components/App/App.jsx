905b75992d8ebadf1aa33c092653b184cdc8cee7
dmck0y <dmckoy85@gmail.com>
2016-10-30 18:59:51 -0400

    Tweak intro animation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,15 +12,20 @@ const App = React.createClass({
     this.domLoaded();
     return (
       <div className='txt-white fs-1rem'>
-        <div className='bc-swamp-black vh-100 w-two-thirds o-hidden content' />
-        <div className='bc-swamp-black-tint
-                        vh-100
-                        w-two-thirds
-                        bg-content
-                        p-abs
-                        top-0
-                        zi-neg100'
-        />
+        <div className='wipe bc-swamp-black-tint vh-100 w-two-thirds p-abs top-0 zi-neg100' />
+        <div className='content-wipe bc-swamp-black vh-100 w-two-thirds p-abs top-0 zi-neg100' />
+        <div className='show-content w-two-thirds pad-lt-10pct of-hidden mar-tp-25vh'>
+          <h1>Hello There.</h1>
+          <h1>I'm David McKoy.</h1>
+          <p>I’m a Designer / Frontend Developer at DOZR. I’ve previously</p>
+          <p>worked with some awesome people at reebee and bitHound.</p>
+          <button>Say Hello</button>
+          <nav>
+            <a href=''>Twitter</a>
+            <a href=''>Dribbble</a>
+            <a href=''>GitHub</a>
+          </nav>
+        </div>
       </div>
     );
   }
